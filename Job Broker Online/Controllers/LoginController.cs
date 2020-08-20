ce1040efb8c0ae6b7d2e13257f96e149ccc81ed7
Yuqing <Yuqingbruce@qq.com>
2020-08-20 17:08:51 +1200

    Add project files.

---
+++ Job Broker Online/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Job_Broker_Online.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Admin Page
+        public ActionResult AdminPage()
+        {
+            return View();
+        }
+
+        // Employee Page
+        public ActionResult EmployeePage()
+        {
+            return View();
+        }
+
+        // Student Page
+        public ActionResult StudentPage()
+        {
+            return View();
+        }
+    }
+}
